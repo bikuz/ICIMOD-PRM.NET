c791fb5ee58089d26bd2cd0fb86474867e4caf87
Bikram <bikuzmail@yahoo.com>
2016-08-02 17:05:48 +0545

    - Add InititialAgreementController - Add PartnershipAgreementController

---
+++ prm/Controllers/InitialAgreementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class InitialAgreementController : Controller
+    {
+        //
+        // GET: /InitialAgreement/
+
+        public ActionResult AddDonor()
+        {
+            return View();
+        }
+
+        public ActionResult AddClauses()
+        {
+            return View();
+        }
+    }
+}
