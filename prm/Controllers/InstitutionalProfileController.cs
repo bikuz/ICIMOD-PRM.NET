1642d209a35066e703269cf24d09a4587adb8c95
Bikram <bikuzmail@yahoo.com>
2016-08-02 11:44:03 +0545

    INstitutionalProfileController and AddPartner.cshtml file added

---
+++ prm/Controllers/InstitutionalProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class InstitutionalProfileController : Controller
+    {
+        //
+        // GET: /InstitutionalProfile/
+
+        public ActionResult AddPartner()
+        {
+            return View();
+        }
+
+        public ActionResult AddPartnerInfo()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AddOrganizationalInfo()
+        {
+            return PartialView();
+        }
+    }
+}
