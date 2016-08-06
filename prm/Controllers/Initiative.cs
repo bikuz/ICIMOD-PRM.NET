d910ca0ae84bd82cbaa10ccd9c1c9ac7f946eb78
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-06 06:49:48 -0700

    Added Initiative-View Profile Page

---
+++ prm/Controllers/Initiative.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class InitiativeController : Controller
+    {
+        //
+        // GET: /Initiative/
+
+        public ActionResult ViewProfile()
+        {
+            return View();
+        }
+
+    }
+}
