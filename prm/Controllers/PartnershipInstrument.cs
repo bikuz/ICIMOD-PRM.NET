f28aa3efae989a565ad878611a650e1982b7cd48
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-06 00:19:55 -0700

    Added Partnership Profile Page

---
+++ prm/Controllers/PartnershipInstrument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class PartnershipInstrumentController : Controller
+    {
+        //
+        // GET: /PartnershipInstrument/
+
+        public ActionResult PartnershipProfile()
+        {
+            return View();
+        }
+
+    }
+}
