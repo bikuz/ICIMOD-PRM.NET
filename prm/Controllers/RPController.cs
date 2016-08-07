ff6fa1c655d3685428fba4fe18908e3527d247d8
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-07 15:08:17 -0700

    Added viewRP page

---
+++ prm/Controllers/RPController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class RPController : Controller
+    {
+        //
+        // GET: /RP/
+
+        public ActionResult ViewRP()
+        {
+            return View();
+        }
+
+    }
+}
