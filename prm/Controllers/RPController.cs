c196a788ad220db336f3f43a86815f4ba08a512c
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-07 15:28:44 -0700

    Added RP Profile page

--- prm/Controllers/RPController.cs
+++ prm/Controllers/RPController.cs
@@ -15,6 +15,10 @@ namespace prm.Controllers
         {
             return View();
         }
+        public ActionResult ViewProfile()
+        {
+            return View();
+        }
 
     }
 }
