e0e13f6a52f4badf96d22b4129a2ed504ec7db8c
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-07 15:13:43 -0700

    Added ViewRMC page

---
+++ prm/Controllers/RMCController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class RMCController : Controller
+    {
+        //
+        // GET: /RMC/
+
+        public ActionResult ViewRMC()
+        {
+            return View();
+        }
+
+    }
+}
