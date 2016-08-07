32789b13f60c030de2e67bd2ca8fc4cedf70e2b1
abhimanyu <er.abhimanyuarora@gmail.com>
2016-08-06 19:18:49 -0700

    Added other controller and Reports upload view

---
+++ prm/Controllers/Other.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prm.Controllers
+{
+    public class OtherController : Controller
+    {
+        //
+        // GET: /Other/
+
+        public ActionResult RMC_RP_InitiativeReportsUpload()
+        {
+            return View();
+        }
+
+    }
+}
